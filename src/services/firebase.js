af63da5d532834865faf86289047a648a1003dcb
Pedro Oliveira <pedrovinicius35@hotmail.com>
2022-01-18 13:53:00 -0300

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDQIVW90Gy2qoIDKp2B8CMb2VtLzFCMdMQ",
-    authDomain: "app-marvel-e7613.firebaseapp.com",
-    projectId: "app-marvel-e7613",
-    storageBucket: "app-marvel-e7613.appspot.com",
-    messagingSenderId: "132798906191",
-    appId: "1:132798906191:web:c2aef00d786721e0bb4f3a"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
