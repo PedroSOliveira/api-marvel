9cd37146fafe5a496f8c1401107b155287f0842d
Pedro Oliveira <pedrovinicius35@hotmail.com>
2022-01-18 13:55:20 -0300

    Update Comics.js

--- src/services/Comics.js
+++ src/services/Comics.js
@@ -3,24 +3,15 @@ import axios from 'axios';
 
 const BASE_URL = 'https://gateway.marvel.com/v1/public/comics';
 
-const PUBLIC_KEY = '2ce20b7ffa510ed2f930b705171627b0';
-const PRIVATE_KEY = 'f03d78536786b17573c5a30739705a5fcb049130';
-
+const PUBLIC_KEY = '';
+const PRIVATE_KEY = '';
 const time = Number(new Date());
-// pub_key = 2ce20b7ffa510ed2f930b705171627b0
 const hash = md5(time + PRIVATE_KEY + PUBLIC_KEY);
-
-// const limit = 8;
-// const offset = 32;
-
-// private_key = f03d78536786b17573c5a30739705a5fcb049130
 
 export const ServiceApiComics = {
 
   getAllComics: async (limit, offset, title) => {
     try {
-
-      // const response = await axios.get(`${BASE_URL}?ts=${time}&apikey=${PUBLIC_KEY}&hash=${hash}&limit=${limit}&offset=${offset}`);
       let url = BASE_URL;
       if(title) url += `?title=${title}`;
       
@@ -28,7 +19,6 @@ export const ServiceApiComics = {
         params: { ts: time, apikey: PUBLIC_KEY, hash, limit, offset }
       });
       const comics = response.data.data;
-      // console.log(response);
 
       return comics;
 
@@ -44,8 +34,6 @@ export const ServiceApiComics = {
         `${BASE_URL}/${comicId}?ts=${time}&apikey=${PUBLIC_KEY}&hash=${hash}`);
 
       const comic = response.data.data.results.shift();
-
-      // const { results: [comic, _ ] } = response.data.data;
 
       console.log(response);
       console.log(comic);
